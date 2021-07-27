693d392f3d4b44f65fcbe4018240d1703c82b78b
arkadi <arkadi@smartbit.be>
2021-07-27 13:50:29 +0200

    .

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -32,7 +32,7 @@ class php::cli (
     }
   }
 
-  $real_settings = lookup('php::cli::settings', Hash, { 'strategy' => 'deep', 'merge_hash_arrays' => true }, $settings)
+  $real_settings = deep_merge($settings, hiera_hash('php::cli::settings', {}))
 
   if $inifile != $php::params::config_root_inifile {
     # only create a cli specific inifile if the filenames are different
